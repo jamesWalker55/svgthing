43522471760b17107561b498e6683c4fa10f90fa
James Walker <james.chunho@gmail.com>
2024-04-16 22:23:50 +0800

    more parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,10 @@
 use nom::{
+    branch::alt,
     bytes::complete::tag,
-    character::complete::{char, space0, u8},
-    combinator::cut,
-    sequence::{delimited, tuple},
+    character::complete::{char, one_of, space0, u8},
+    combinator::{cut, opt, recognize},
+    multi::count,
+    sequence::{delimited, preceded, tuple},
     IResult, Parser,
 };
 
@@ -10,41 +12,85 @@ type Input = str;
 
 type Result<'a, O = &'a Input> = IResult<&'a Input, O>;
 
+#[derive(PartialEq, Eq, Debug)]
+enum Color {
+    RGB(u8, u8, u8),
+    RGBA(u8, u8, u8, u8),
+}
+
 fn color_rgb_value(input: &Input) -> Result<u8> {
     delimited(space0, u8, space0)(input)
 }
 
-fn color_rgb(input: &Input) -> Result<(u8, u8, u8)> {
-    delimited(
-        tag("rgb("),
-        cut(tuple((
-            color_rgb_value,
-            char(','),
-            color_rgb_value,
-            char(','),
-            color_rgb_value,
-        ))),
-        cut(char(')')),
-    )
-    .map(|(r, _, g, _, b)| (r, g, b))
-    .parse(input)
+fn color_numeric(input: &Input) -> Result<Color> {
+    alt((
+        delimited(
+            tag("rgba("),
+            cut(tuple((
+                color_rgb_value,
+                char(','),
+                color_rgb_value,
+                char(','),
+                color_rgb_value,
+                char(','),
+                color_rgb_value,
+            ))),
+            cut(char(')')),
+        )
+        .map(|(r, _, g, _, b, _, a)| Color::RGBA(r, g, b, a)),
+        delimited(
+            tag("rgb("),
+            cut(tuple((
+                color_rgb_value,
+                char(','),
+                color_rgb_value,
+                char(','),
+                color_rgb_value,
+            ))),
+            cut(char(')')),
+        )
+        .map(|(r, _, g, _, b)| Color::RGB(r, g, b)),
+    ))(input)
 }
 
-fn color_rgba(input: &Input) -> Result<(u8, u8, u8, u8)> {
-    delimited(
-        tag("rgba("),
-        cut(tuple((
-            color_rgb_value,
-            char(','),
-            color_rgb_value,
-            char(','),
-            color_rgb_value,
-            char(','),
-            color_rgb_value,
-        ))),
-        cut(char(')')),
+fn color_hex(input: &Input) -> Result<Color> {
+    preceded(
+        char('#'),
+        tuple((
+            recognize(tuple((
+                one_of("0123456789abcdefABCDEF"),
+                one_of("0123456789abcdefABCDEF"),
+            ))),
+            recognize(tuple((
+                one_of("0123456789abcdefABCDEF"),
+                one_of("0123456789abcdefABCDEF"),
+            ))),
+            recognize(tuple((
+                one_of("0123456789abcdefABCDEF"),
+                one_of("0123456789abcdefABCDEF"),
+            ))),
+            opt(recognize(tuple((
+                one_of("0123456789abcdefABCDEF"),
+                one_of("0123456789abcdefABCDEF"),
+            )))),
+        )),
     )
-    .map(|(r, _, g, _, b, _, a)| (r, g, b, a))
+    .map(|(r, g, b, a)| {
+        let r =
+            u8::from_str_radix(r, 16).expect(format!("failed to convert {r} to number").as_str());
+        let g =
+            u8::from_str_radix(g, 16).expect(format!("failed to convert {g} to number").as_str());
+        let b =
+            u8::from_str_radix(b, 16).expect(format!("failed to convert {b} to number").as_str());
+        let a = a.map(|x| {
+            u8::from_str_radix(x, 16).expect(format!("failed to convert {b} to number").as_str())
+        });
+        if let Some(a) = a {
+            Color::RGBA(r, g, b, a)
+        } else {
+            Color::RGB(r, g, b)
+        }
+    })
     .parse(input)
 }
 
@@ -53,17 +99,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_color_rgb() {
+    fn test_color_numeric() {
         // spacing
-        assert_eq!(color_rgb("rgb(1,2,3)").unwrap().1, (1, 2, 3));
-        assert_eq!(color_rgb("rgb(1, 2, 3)").unwrap().1, (1, 2, 3));
-        assert_eq!(color_rgb("rgb(  1  ,  2  ,  3  )").unwrap().1, (1, 2, 3));
-        assert!(color_rgb("rgb(  1  ,  2  ,  3  ,)").is_err());
-        assert!(color_rgb("rgb (1, 2, 3)").is_err());
+        assert_eq!(color_numeric("rgb(1,2,3)").unwrap().1, Color::RGB(1, 2, 3));
+        assert_eq!(
+            color_numeric("rgb(1, 2, 3)").unwrap().1,
+            Color::RGB(1, 2, 3)
+        );
+        assert_eq!(
+            color_numeric("rgb(  1  ,  2  ,  3  )").unwrap().1,
+            Color::RGB(1, 2, 3)
+        );
+        assert!(color_numeric("rgb(  1  ,  2  ,  3  ,)").is_err());
+        assert!(color_numeric("rgb (1, 2, 3)").is_err());
+
+        assert_eq!(
+            color_numeric("rgba(1,2,3,4)").unwrap().1,
+            Color::RGBA(1, 2, 3, 4)
+        );
+        assert_eq!(
+            color_numeric("rgba(1, 2, 3, 4)").unwrap().1,
+            Color::RGBA(1, 2, 3, 4)
+        );
+        assert_eq!(
+            color_numeric("rgba(  1  ,  2  ,  3  ,  4  )").unwrap().1,
+            Color::RGBA(1, 2, 3, 4)
+        );
+        assert!(color_numeric("rgba(  1  ,  2  ,  3  ,  4  ,)").is_err());
+        assert!(color_numeric("rgba (1, 2, 3,4)").is_err());
 
         // valid numbers
-        assert_eq!(color_rgb("rgb(0, 100, 255)").unwrap().1, (0, 100, 255));
-        assert!(color_rgb("rgb(0, 256, 0)").is_err());
-        assert!(color_rgb("rgb(-1, 0, 0)").is_err());
+        assert_eq!(
+            color_numeric("rgb(0, 100, 255)").unwrap().1,
+            Color::RGB(0, 100, 255)
+        );
+        assert!(color_numeric("rgb(0, 256, 0)").is_err());
+        assert!(color_numeric("rgb(-1, 0, 0)").is_err());
     }
 }
